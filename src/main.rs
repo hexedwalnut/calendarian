6914c5e89ea5df577f7058f08a4f0d535d8ed793
hexedwalnut <jteahe02@gmail.com>
2024-01-22 12:05:14 -0500

    Basic Structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+struct Calendar {
+    cycles: Vec<Cycle>,
+    year_length_hours: i64,
+}
+
+struct Cycle {
+    cycle_name: String,
+    cycle_length_hours: i64,
+}
+
 fn main() {
     println!("Hello, world!");
 }
